c97448438816d0edd48b78467695a673c5ebd4f9
Yuya Miyajima <you@example.com>
2020-04-13 01:51:37 +0900

    add_post_detail implement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,15 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
 
-  def new
-    @post = Post.new
-  end
-
-  def edit
+  def show
     @post = Post.find(params[:id])
   end
 
-  def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to posts_path
+  def new
+    @post = Post.new
   end
 
   def create
