7875fc29a3eb68505da396effe07a0b416e94a2d
Yuya Miyajima <you@example.com>
2020-04-12 11:49:20 +0900

    rails6 bulkinsert

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
-categories = []
-['Ruby', 'Rails', 'HTML5・CSS3', 'JavaScript', 'IT用語', 'お役立ち情報', 'その他'].each do |category_name|
-  category = Category.new(name: category_name)
-  categories << category
-end
-Category.import categories
+Category.insert_all!([
+  { id: 1, name: 'Ruby', created_at: Time.zone.now, updated_at: Time.zone.now },
+  { id: 2, name: 'Rails', created_at: Time.zone.now, updated_at: Time.zone.now },
+  { id: 3, name: 'HTML5・CSS3', created_at: Time.zone.now, updated_at: Time.zone.now },
+  { id: 4, name: 'JavaScript', created_at: Time.zone.now, updated_at: Time.zone.now },
+  { id: 5, name: 'IT用語', created_at: Time.zone.now, updated_at: Time.zone.now },
+  { id: 6, name: 'お役立ち情報', created_at: Time.zone.now, updated_at: Time.zone.now },
+  { id: 7, name: 'その他', created_at: Time.zone.now, updated_at: Time.zone.now }
+])
