dc5a4bd86398176a5ac12c100ca7adcd73ddc5ab
Yuya Miyajima <you@example.com>
2020-04-12 13:33:13 +0900

    fix migration

--- db/migrate/20200412020455_add_category_id_to_posts.rb
+++ db/migrate/20200412020455_add_category_id_to_posts.rb
@@ -1,7 +1,5 @@
 class AddCategoryIdToPosts < ActiveRecord::Migration[6.0]
   def change
-    add_column :posts, :category_id, :bigint
-    add_foreign_key :posts, :categories
-    add_index  :posts,  :category_id
+    add_reference :posts, :category
   end
 end
