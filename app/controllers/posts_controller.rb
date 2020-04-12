fcdc1f470ac3b168f7930bcc0d2f7118ceb91446
Yuya Miyajima <you@example.com>
2020-04-12 13:39:45 +0900

    create_category implement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :url, :description)
+    params.require(:post).permit(:title, :url, :description, :category_id)
   end
 end
