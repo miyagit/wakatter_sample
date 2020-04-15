29c040d03c171375e016e41761f2ebb43443dcf3
Yuya Miyajima <you@example.com>
2020-04-16 04:48:19 +0900

    fix_migration_file implement

--- db/migrate/20200411071548_create_posts.rb
+++ db/migrate/20200411071548_create_posts.rb
@@ -4,8 +4,6 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :url
       t.text   :description
-      t.references :user, foreign_key: true
-
       t.timestamps
     end
   end
