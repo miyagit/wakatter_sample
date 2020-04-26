fc6fb95eec317785e7d04dc97ecc30568fde60ce
Yuya Miyajima <you@example.com>
2020-04-26 11:25:24 +0900

    right_sidebar_group implement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @sidebar_posts = Post.joins(:post_likes).group("post_id").order('count_all DESC').count.keys.map{ |post_id| Post.find(post_id) }
   end
 
   def show
@@ -25,6 +26,7 @@ class PostsController < ApplicationController
 
   def update
     authenticate_user!
+    @post = Post.find(params[:id])
     redirect_to root_path unless current_user == @post.user
     post = Post.find(params[:id])
     post.update(post_params)
